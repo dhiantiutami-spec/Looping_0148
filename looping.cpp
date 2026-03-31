6ebc72264c6d7c1ecc1c3e6b428fb3d40c71b555
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-03-31 19:10:21 +0700

    menambahkan logikan AND

--- looping.cpp
+++ looping.cpp
@@ -73,4 +73,14 @@ int main(){
     cin>>nilB;
 
     rerata = (nilB+nilM)/2;
+
+    if((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
+
+//Logika AND
+#include <iostream>
+using namespace std;
